6bf3ba5058d0aacab55560a52e82496b3f363470
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-11-07 13:01:10 -0800

    added markers and infoWindows to the mapContainer

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './pages/common/components/Navbar';
-import TestSite from '../src/pages/common/components/TestSite'
+import TestSite from '../src/pages/common/components/TestSite';
+
 
 import {
   WrappedSignUp,
   WrappedSignIn,
 } from './pages/Viewer';
 
-
-
 function App() {
   return (
     <Router>
-      <Navbar/>
-      <Route path='/testsite' component={TestSite}/>
-      <Route path='/signup' component={WrappedSignUp}/>
-      <Route path='/signin' component={WrappedSignIn}/>
+      <Navbar />
+      <Route path='/testsite' component={TestSite} />
+      <Route path='/signup' component={WrappedSignUp} />
+      <Route path='/signin' component={WrappedSignIn} />
       <Route exact path="/">
         <h1>Welcome to the about</h1>
       </Route>
--- client/src/pages/common/components/TestSite.js
+++ client/src/pages/common/components/TestSite.js
@@ -1,47 +1,14 @@
 import React from 'react';
-// import Card from './Card';
-// import MatchBar from './MatchBar';
-// import { useSelector } from 'react-redux';
-// import { useTestSite } from '../hooks/UseTestSites';
-import API from '../../../utils/API';
-import MapContainer from './MapContainer'
+import MapContainer from './MapContainer';
 
-// const Discover = () => {
-
-//   useEffect(() => {
-//     loadAPI();
-//   }, []);
-
-//   const loadAPI = () => {
-//     API.search()
-//   };
-//   return (
-//     <div>
-//       <h1>Hi</h1>
-//     </div>
-//   );
-// };
-class Discover extends React.Component {
-
-  async componentDidMount() {
-    const coords = await this.loadAPI();
-    console.log('I am coords', coords);
-    let positionArray = []
-    coords.map(coordinate => positionArray.push({lat:coordinate.point[1], lng:coordinate.point[0]} ));
-    console.log('Position Array', positionArray);
-  }
-
-  loadAPI = async() => {
-    return await API.search()
-  };
+class TestSite extends React.Component {
 
   render() {
+
     return (
-        <MapContainer>
-          
-        </MapContainer>
+      <MapContainer />
     );
   }
 }
 
-export default Discover;
+export default TestSite;
