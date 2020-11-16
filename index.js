7917c456b47fc9f390ced6e8b1d3eb9730211987
Harbour Harrison <harb3gaucho@gmail.com>
2020-11-16 12:37:27 -0800

    changed index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  // app.get("/*", function (req, res) {
-  //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  // });
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
