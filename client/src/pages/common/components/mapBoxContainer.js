09e6489653a964bdc235621c22b0ff93a13ed8da
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-11-23 21:59:43 -0800

    added sidebar with Longitude and Latitude coordinates

--- client/src/pages/common/components/mapBoxContainer.js
+++ client/src/pages/common/components/mapBoxContainer.js
@@ -20,11 +20,21 @@ class Application extends React.Component {
             center: [this.state.lng, this.state.lat],
             zoom: this.state.zoom
         });
+        map.on('move', () => {
+            this.setState({
+                lng: map.getCenter().lng.toFixed(4),
+                lat: map.getCenter().lat.toFixed(4),
+                zoom: map.getZoom().toFixed(2)
+            });
+        });
     }
     render() {
         return (
             <div>
-                <div ref={el => this.mapContainer = el} className="mapContainer" />
+                <div className='sidebarStyle'>
+                    <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+                </div>
+                <div ref={el => this.mapContainer = el} className='mapContainer' />
             </div>
         )
     }
