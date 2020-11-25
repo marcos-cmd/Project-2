d0fbda935e269bff7c94642a389f8037f40e2681
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-11-24 19:38:05 -0800

    added testSite markers

--- client/src/pages/common/components/mapBoxContainer.js
+++ client/src/pages/common/components/mapBoxContainer.js
@@ -1,24 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl';
+import testingLocations from './'
+import API from '../../../utils/API';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
+
+const dataUrl = "https://data.sfgov.org/resource/dtit-7gp4.geojson?$where=point+is+not+null";
+
+// let locations;
+
+// fetch(dataUrl)
+//     .then(function (response) {
+//         if (!response.ok) {
+//             throw new Error
+//         }
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         locations = data;
+//         return;
+//     });
+
+
 
 class Application extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            lng: 5,
-            lat: 34,
-            zoom: 2
+            lng: -122.431297,
+            lat: 37.773972,
+            zoom: 12,
+            bounds: [-122.517910874663, 37.6044780500533, -122.354995082683, 37.8324430069081],
+            coords: [],
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
+            address: "",
         };
     }
-    componentDidMount() {
+    // This function loads the API Request
+    loadAPI = async () => {
+        return await API.search()
+    };
+
+    async componentDidMount() {
         const map = new mapboxgl.Map({
             container: this.mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v11',
+            style: 'mapbox://styles/mapbox/dark-v10',
             center: [this.state.lng, this.state.lat],
-            zoom: this.state.zoom
+            zoom: this.state.zoom,
+            maxBounds: this.state.bounds
         });
         map.on('move', () => {
             this.setState({
@@ -27,7 +59,40 @@ class Application extends React.Component {
                 zoom: map.getZoom().toFixed(2)
             });
         });
-    }
+        map.on('load', function (e) {
+            map.addSource('covid cases', {
+                type: 'geojson',
+                data: dataUrl
+            });
+        })
+
+        const coords = await this.loadAPI();
+        // console.log('I am coords', coords);
+        let positionArray = []
+        // positionArray will become the empty array 'coords', 
+        // which is waiting empty in state at this point
+        coords.map(coordinate => positionArray.push(
+            {
+                lat: coordinate.point[1],
+                lng: coordinate.point[0],
+                id: coordinate.id,
+                address: coordinate.address,
+                name: coordinate.name,
+                activeMarker: null,
+                showingInfoWindow: false
+            }));
+        // this line sets position array as the array 'coords' in our state
+        this.setState({ coords: positionArray })
+
+        const createMarker = () => {
+            this.state.coords.map((coord, index) => {
+                const marker = new mapboxgl.Marker()
+                    .setLngLat([coord.lng, coord.lat])
+                    .addTo(map);
+            });
+        };
+        createMarker();
+    };
     render() {
         return (
             <div>
