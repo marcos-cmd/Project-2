a2633cbf0004b0e7a24ba3c1b76c67759cbc8c65
Lindsey Smith <campbelllm@gmail.com>
2020-11-28 10:37:58 -0700

    sign in and sign up pages working and styled

--- client/src/pages/Viewer/ViewerViews/SignIn.js
+++ client/src/pages/Viewer/ViewerViews/SignIn.js
@@ -1,10 +1,77 @@
-import React from 'react';
-import { reduxForm, Field } from 'redux-form';
-import TextField from '@material-ui/core/TextField';
-import axios from 'axios';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { reduxForm, Field } from "redux-form";
+import TextField from "@material-ui/core/TextField";
+import axios from "axios";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Link from "@material-ui/core/Link";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Navbar from "../../common/components/Navbar";
+import { setViewerToken } from "../ViewerReducer";
 
-import { setViewerToken } from '../ViewerReducer';
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+   fontFamily: "Raleway, sans-serif",
+    margin:'300px auto',
+  },
+  image: {
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(0, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    boxShadow: '2px 2px 6px 0px grey',
+    padding: '20px',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: '#cfcfcf',
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: "#455A64",
+    color: '#cfcfcf',
+    fontFamily: "Raleway, sans-serif",
+    fontWeight: 'bold',
+    fontSize: "20px"
+  },
+  font: {
+    fontFamily: "Raleway, sans-serif",
+  }
+}));
+
+
 // The Field components job is to render out input html
 // and pass down functions for updating the state
 // as well as check to see if the values being passed are valid
@@ -21,12 +88,9 @@ import { setViewerToken } from '../ViewerReducer';
 //   />;
 // };
 
-const TextFieldInput = ({ input, meta, label }) => {
+const TextFieldInput = ({ input, meta, label, ...custom }) => {
   // console.log('FIELD COMPONENT PROPS', props);
-  return <TextField
-    {...input}
-    label={label}
-  />;
+  return <TextField {...input} label={label} meta={meta} {...custom} />;
 };
 
 // What Redux form does for us
@@ -40,42 +104,81 @@ const TextFieldInput = ({ input, meta, label }) => {
 // to the function that it's calling
 const SignIn = (props) => {
   const { handleSubmit, history } = props;
+  const classes = useStyles();
 
   console.log(props);
   const handleSignIn = async (formValues, dispatch) => {
     console.log(formValues);
     //{ username: 'Your enterereduseRName', password: 'your password' }
     try {
-      const res = await axios.post('/auth/signin', formValues);
-      localStorage.setItem('token', res.data);
-      localStorage.setItem('username', formValues.username)
+      const res = await axios.post("/auth/signin", formValues);
+      localStorage.setItem("token", res.data);
+      localStorage.setItem("username", formValues.username);
       dispatch(setViewerToken(res.data));
       history.push(`/Profile/${formValues.username}`);
     } catch (e) {
       throw new Error(e);
     }
-  }
+  };
 
   return (
-    <form noValidate autoComplete="off">
-      <Field
-        name='username'
-        label='username'
-        component={TextFieldInput}
-      />
-      <Field
-        name='password'
-        label='password'
-        component={TextFieldInput}
-      />
-      <Button
-        onClick={handleSubmit(handleSignIn)}
-        variant="contained"
-        color="primary">
-        Sign in
-      </Button>
-    </form>
+    <Grid container component="main" >
+      <CssBaseline />
+      <Navbar />
+      {/* <Grid item xs={false} sm={4} md={7} className={classes.image} /> */}
+      <Grid item xs={12} sm={8} md={5} className={classes.root}>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5" className={classes.font}>
+            SIGN IN
+          </Typography>
+          <form noValidate autoComplete="off" className={classes.form}>
+            <Field
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="username"
+              label="username"
+              component={TextFieldInput}
+              autoFocus
+              className={classes.font}
+            />
+            <Field
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="password"
+              component={TextFieldInput}
+              type="password"
+              className={classes.font}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              onClick={handleSubmit(handleSignIn)}
+              variant="contained"
+              className={classes.submit}
+            >
+              SIGN IN
+            </Button>
+            <Grid item>
+              <Link href="/signup" variant="body2" className={classes.font}>
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+            <Box mt={5}>
+              <Copyright className={classes.font} />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
-export const WrappedSignIn = reduxForm({ form: 'signInForm' })(SignIn);
+export const WrappedSignIn = reduxForm({ form: "signInForm" })(SignIn);
--- client/src/pages/Viewer/ViewerViews/SignUp.js
+++ client/src/pages/Viewer/ViewerViews/SignUp.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
-import TextField from '@material-ui/core/TextField';
-import axios from 'axios';
-import Button from '@material-ui/core/Button';
-import Navbar from '../../common/components/Navbar';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { setViewerToken } from '../ViewerReducer';
-import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import TextField from "@material-ui/core/TextField";
+import axios from "axios";
+import Button from "@material-ui/core/Button";
+import Navbar from "../../common/components/Navbar";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { setViewerToken } from "../ViewerReducer";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
 // The Field components job is to render out input html
 // and pass down functions for updating the state
 // as well as check to see if the values being passed are valid
@@ -34,12 +32,9 @@ import Container from '@material-ui/core/Container';
 //   />;
 // };
 
-const TextFieldInput = ({ input, meta, label }) => {
+const TextFieldInput = ({ input, meta, label, ...custom }) => {
   // console.log('FIELD COMPONENT PROPS', props);
-  return <TextField
-    {...input}
-    label={label}
-  />;
+  return <TextField {...input} label={label} meta={meta} {...custom} />;
 };
 
 // What Redux form does for us
@@ -53,119 +48,136 @@ const TextFieldInput = ({ input, meta, label }) => {
 // to the function that it's calling
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    margin: '300px auto 0 auto',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    boxShadow: '2px 2px 6px 0px grey',
+    padding: '25px',
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: "#cfcfcf",
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
+    backgroundColor: "#455A64",
+    color: "#cfcfcf",
+    fontFamily: "Raleway, sans-serif",
+    fontWeight: "bold",
+    fontSize: "20px",
   },
-  // test:{
-  //   margin: '500px',
-  // }
+  font: {
+    fontFamily: "Raleway, sans-serif",
+    fontWeight: "normal"
+  },
+  test:{
+   boxShadow: '5px 5px 5px rgba',
+  }
 }));
-
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Covid Tracking
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
-} 
+}
 
 export default function SignUp(props) {
-  const classes = useStyles()
+  const classes = useStyles();
 
-   const handleSignUp = async (formValues) => {
+  const handleSignUp = async (formValues) => {
     console.log(formValues);
     //{ username: 'Your enterereduseRName', password: 'your password' }
     try {
-      const res = await axios.post('/auth/signup', formValues);
-      console.log('I AM THE SIGNUP USERS TOKEN', res.data);
-      localStorage.setItem('token', res.data);
-      localStorage.setItem('username', formValues.username)
+      const res = await axios.post("/auth/signup", formValues);
+      console.log("I AM THE SIGNUP USERS TOKEN", res.data);
+      localStorage.setItem("token", res.data);
+      localStorage.setItem("username", formValues.username);
       props.setViewerToken(res.data);
       props.history.push(`/Profile/${formValues.username}`);
       // sessionStorage.setItem('token', res.data);
     } catch (e) {
       throw new Error(e);
     }
-  }
-   
-    const { handleSubmit } = props;
-    return (
-      <div>
-        <div>
-      <Navbar/>
-        </div>
-      <Container className={classes.test} component="main" maxWidth="xs">
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-       
-      <form className={classes.form} noValidate autoComplete="off">
-      <Grid container spacing={2}>
-      <Grid item xs={12} sm={6}>
-        <Field
-          name='username'
-          label='username'
-          component={TextFieldInput}
-        />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <Field
-          name='password'
-          label='password'
-          component={TextFieldInput}
-        />
-        </Grid>
-        <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        className={classes.submit}
-          onClick={handleSubmit(handleSignUp)}>
-          Sign up
-        </Button>
-        <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </Grid>
-      </form>
-      
-      </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
-      </Container>
-      </div>
-    );
-  
-}
+  };
 
+  const { handleSubmit } = props;
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <Container className={classes.test} component="main" maxWidth="sm">
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5" className={classes.font}>
+            SIGN UP
+          </Typography>
+
+          <form className={classes.form} noValidate autoComplete="off">
+            <Grid container spacing={2}>
+              <Field
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="username"
+                label="username"
+                component={TextFieldInput}
+                className={classes.font}
+              />
+
+              <Field
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="password"
+                autoComplete="password"
+                type="password"
+                component={TextFieldInput}
+                className={classes.font}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                className={classes.submit}
+                onClick={handleSubmit(handleSignUp)}
+              >
+                SIGN UP
+              </Button>
+
+              <Grid item>
+                <Link href="/signin" variant="body2" className={classes.font}>
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+          <Copyright className={classes.font} />
+        </Box>
+          </form>
+        </div>
+       
+      </Container>
+    </div>
+  );
+}
 
 // const SignUp = (props) => {
 //   const { handleSubmit, history } = props;
@@ -208,7 +220,7 @@ export default function SignUp(props) {
 // };
 function mapStateToProps(state) {
   return { superman: state.viewer };
-};
+}
 
 // mapDispatchToProps
 
@@ -217,10 +229,9 @@ function mapStateToProps(state) {
 //
 // export const WrappedSignUp = reduxForm({ form: 'signUpForm' })(composedComponent);
 
-
 // export const WrappedSignUp = reduxForm({ form: 'signUpForm' })(connect(mapStateToProps, { setUserToken })(SignUp));
 
 export const WrappedSignUp = compose(
-  reduxForm({ form: 'signUpForm' }),
+  reduxForm({ form: "signUpForm" }),
   connect(mapStateToProps, { setViewerToken })
 )(SignUp);
