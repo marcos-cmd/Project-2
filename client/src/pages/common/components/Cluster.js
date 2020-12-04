e83d9434510d9cf2a150182efa8e567c7d05f18d
Harbour Harrison <harb3gaucho@gmail.com>
2020-12-03 16:31:21 -0800

    changed cluster coloring system

--- client/src/pages/common/components/Cluster.js
+++ client/src/pages/common/components/Cluster.js
@@ -58,9 +58,9 @@ class Cluster extends React.Component {
             // Add a new source from our GeoJSON data and
             // set the 'cluster' option to true. GL-JS will
             // add the point_count property to your source data.
-            map.addSource('earthquakes', {
+            map.addSource('covidPoints', {
                 type: 'geojson',
-                // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+                // Point to GeoJSON data. This example visualizes all M1.0+ covidPoints
                 // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
                 data: {
                     "type": "FeatureCollection",
@@ -68,14 +68,14 @@ class Cluster extends React.Component {
                     "features": points
                 },
                 cluster: true,
-                clusterMaxZoom: 14, // Max zoom to cluster points on
+                clusterMaxZoom: 15, // Max zoom to cluster points on
                 clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
             });
 
             map.addLayer({
                 id: 'clusters',
                 type: 'circle',
-                source: 'earthquakes',
+                source: 'covidPoints',
                 filter: ['has', 'point_count'],
                 paint: {
                     // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
@@ -86,11 +86,11 @@ class Cluster extends React.Component {
                     'circle-color': [
                         'step',
                         ['get', 'point_count'],
-                        '#51bbd6',
-                        100,
-                        '#f1f075',
-                        750,
-                        '#f28cb1'
+                        'green',
+                        3,
+                        'orange',
+                        7,
+                        'red'
                     ],
                     'circle-radius': [
                         'step',
@@ -107,7 +107,7 @@ class Cluster extends React.Component {
             map.addLayer({
                 id: 'cluster-count',
                 type: 'symbol',
-                source: 'earthquakes',
+                source: 'covidPoints',
                 filter: ['has', 'point_count'],
                 layout: {
                     'text-field': '{point_count_abbreviated}',
@@ -119,7 +119,7 @@ class Cluster extends React.Component {
             map.addLayer({
                 id: 'unclustered-point',
                 type: 'circle',
-                source: 'earthquakes',
+                source: 'covidPoints',
                 filter: ['!', ['has', 'point_count']],
                 paint: {
                     'circle-color': '#11b4da',
@@ -135,7 +135,7 @@ class Cluster extends React.Component {
                     layers: ['clusters']
                 });
                 var clusterId = features[0].properties.cluster_id;
-                map.getSource('earthquakes').getClusterExpansionZoom(
+                map.getSource('covidPoints').getClusterExpansionZoom(
                     clusterId,
                     function (err, zoom) {
                         if (err) return;
