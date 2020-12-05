fcaa7809ccf636dba53048d0f61df06823590797
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-12-05 11:30:57 -0800

    cleaned up frontEnd code, removing unused imports

--- client/src/pages/common/components/Virus.js
+++ client/src/pages/common/components/Virus.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Sky from 'react-sky';
 import virusImg from '../../../virus.png'
 
@@ -6,20 +6,20 @@ import virusImg from '../../../virus.png'
 
 const Virus = () => {
 
-    return (
-      <div>
-        <Sky
-          images={{
-            0: virusImg, 
-          }}
-          how={150} /* Pass the number of images Sky will render chosing randomly */
-          time={50} /* time of animation */
-          size={'30px'} /* size of the rendered images */
-          
-        />
-      </div>
-    );
-  
+  return (
+    <div>
+      <Sky
+        images={{
+          0: virusImg,
+        }}
+        how={150} /* Pass the number of images Sky will render chosing randomly */
+        time={50} /* time of animation */
+        size={'30px'} /* size of the rendered images */
+
+      />
+    </div>
+  );
+
 }
 
 export default Virus;
