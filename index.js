ce0f0398efb3030a2096d0c33b7089153487d86d
Lindsey Smith <campbelllm@gmail.com>
2020-12-07 21:45:08 -0700

    chaning order of routes, index.js

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ mongoose.set('debug', true);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(routes);
 app.listen(PORT, () => {
   console.log('Server started listening on PORT http://localhost:3001');
 });
