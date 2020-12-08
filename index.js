b5621b3d6bd59ca202fd204f9fa2e7b3b07422c3
Lindsey Smith <campbelllm@gmail.com>
2020-12-07 21:02:29 -0700

    index.js remove *

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get("/*", function (req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
