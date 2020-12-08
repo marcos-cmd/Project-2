3debd401e51668e5af6ee8912d96203eab29fb68
Lindsey Smith <campbelllm@gmail.com>
2020-12-07 21:31:07 -0700

    back to browserRouter, changing route in index.js

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ mongoose.set('debug', true);
 const app = express();
 const PORT = process.env.PORT || 3001;
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  // app.get("*", function (req, res) {
-  //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  // });
+  // app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'build')));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
